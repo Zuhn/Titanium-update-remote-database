c7db98631afc8fae82f761d2fb6d6a045fe935d3
xavier gerber <xg@grafmiville.ch>
2013-12-03 11:19:14 +0100

    New download function

--- update-remote-database.js
+++ update-remote-database.js
@@ -1,25 +1,62 @@
-var xhr = Titanium.Network.createHTTPClient({
-    onload: function() {
-        var db = Ti.Database.open('mydb1Installed');
-    	db.remove();
-        var newdatabase = Ti.Database.install(Titanium.Filesystem.applicationSupportDirectory+'/myalloy.sql', 'mydb1Installed');
-        var db = Ti.Database.open('mydb1Installed');
-    },
-    timeout: 10000
-});
-xhr.open('GET','http://www.processmx.com/alloy.sql');
-xhr.file = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationSupportDirectory,'myalloy.sql');
-xhr.send();
-
-/*
-function reloadData()
+var header_loader = null;
+//Définition du chemin de stocage de la db
+var path_to_remote_file = 'http://www.mondomaine.com/mytest.sql';
+var path_to_doc = Titanium.Filesystem.applicationDataDirectory;
+var xhr;
+function reloadData(e)
 {
-	xhr.file = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationSupportDirectory,'myalloy.sql');
+	xhr = Titanium.Network.createHTTPClient({
+	    onload: function() {
+	    	
+	    	//SUpression de la base local existante
+	    	var db = Ti.Database.open('mydb1Installed');
+	        db.remove();
+	
+	        // delete all items
+	    	Alloy.Collections.voucher.deleteAll();
+	
+	        //var db1 = Ti.Database.install(Ti.Filesystem.getFile(path_to_doc+'myalloy.sql'),'mydb1Installed');
+	        var dbInstall = Ti.Database.install(path_to_doc+'/myalloy.sql', 'mydb1Installed');
+	
+	        //suppression du fichier temporaire
+	        var file = Titanium.Filesystem.getFile(path_to_doc,'myalloy.sql');
+			if (file.exists()) file.deleteFile(); 
+	
+			displayData()
+			xhr = null;
+	
+	    },
+	    timeout: 10000
+	
+	});
+
+	header_loader = e;
+	xhr.open('GET',path_to_remote_file);
+	xhr.file = Titanium.Filesystem.getFile(path_to_doc,'myalloy.sql');
 	xhr.send();
 }
-$.index.addEventListener('click', reloadData);
-*/
 
+function displayData()
+{
+	var db = Ti.Database.open('mydb1Installed');
 
+    //Récupération des données SQLite
+   var product = db.execute("SELECT * FROM voucher" );
+	
+   //Insertion des nouvelles données dans la base
+	while (product.isValidRow())
+	{
+		var new_voucher = Alloy.createModel('voucher', {name:product.fieldByName('name')}); 
+		new_voucher.save();
 
+		product.next();
+	}
+	//Mise à jour de a collection voucher
+	Alloy.Collections.voucher.fetch();
+	if(header_loader != null) header_loader.hide();
+	
+	db.close();
+}
+
+displayData()
 $.index.open();
